cafb13faca26161c97d444f8232f7244e6f1b126
Rowan Dombroskie <rowan.dombroskie1@gmail.com>
2026-09-09 17:11:54 +0000

    YO

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
+const sidebar = document.querySelector(".side_stuff");
+const buttons = document.querySelectorAll("#toggleSidebar");
+
 function popup(){
     alert("V.3");
 }
-function run(){
 
-}
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+    });
+});
