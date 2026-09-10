7c8bb0d9557c0158c0ff533954b11941d1c782bf
Rowan Dombroskie <rowan.dombroskie1@gmail.com>
2026-09-10 18:03:06 +0000

    maybe better?

--- script.js
+++ script.js
@@ -3,32 +3,27 @@ const content = document.getElementById("content");
 const Obutton = document.querySelector("#openSide");
 const Xbutton = document.querySelector("#closeSide");
 
-function openSide(){
+Obutton.addEventListener("click", function () {
     sidebar.classList.add("open");
-}
-function closeSide(){
+});
+
+Xbutton.addEventListener("click", function () {
     sidebar.classList.remove("open");
-}
-Obutton.addEventListener("click", openSide())
-Xbutton.addEventListener("click", closeSide())
+});
 
 
 function showPage(page){
     if (page === 'home'){
         content.innerHTML = `
-            <button id="openSide">O</button>
             <h1>Home</h1>
         `;
     }
     if (page === 'scheduling'){
         content.innerHTML = `
-            <button id="openSide">O</button>
             <h1>Scheduling</h1>
         `;
     }
     if (page === 'ph'){
         
     }
-    Obutton = document.querySelector("#openSide");
-    Obutton.addEventListener("click", openSide())
 }
